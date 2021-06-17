d85e1e8dfa6a4acb5133f379e7a713dee4c15792
julianfoo <julian.foo@investingnote.com>
2021-06-17 22:36:39 +0800

    WIP: Style chart

---
+++ app/controllers/charts_controller.rb
@@ -0,0 +1,8 @@
+class ChartsController < ApplicationController
+  def show
+    derivative_id = params[:id]
+    data = DerivativeDailyHistoricalPrice.where(derivative_id: derivative_id).order(:created_at).limit(7).pluck(:created_at, :price)
+    data = data.collect { |date, price| [date.strftime('%b %d'), price] }
+    render json: data
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :derivatives, only: %i[index show]
+  resources :charts, only: %i[show]
   get 'coins', to: 'coins#index'
   get 'coins/:id', to: 'coins#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
