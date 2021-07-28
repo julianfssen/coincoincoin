4209eddbc7f34f66b8980e837374109391cc1b0a
julianfoo <julian.foo@investingnote.com>
2021-07-28 14:29:52 +0800

    Changed price history mehtod to retrive last 7 days data instead

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,7 +1,7 @@
 class ChartsController < ApplicationController
   def show
     derivative_id = params[:id]
-    data = DerivativeDailyHistoricalPrice.where(derivative_id: derivative_id).order(:created_at).limit(7).pluck(:created_at, :price)
+    data = DerivativeDailyHistoricalPrice.where(derivative_id: derivative_id).order(created_at: :desc).limit(7).pluck(:created_at, :price)
     data = data.collect { |date, price| [date.strftime('%b %d, %Y'), price] }
     render json: data
   end
